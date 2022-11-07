5c1c1e9ffdd269600b864f550ba2bd27b584141c
Naiara Rodrigues <rnaiara92@gmail.com>
2022-11-07 16:56:27 -0300

    import pag n encontrada

--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
-    SegundaPaginaComponent
+    SegundaPaginaComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
